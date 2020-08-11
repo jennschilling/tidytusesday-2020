4744758c542fe34c392a779e2e2b012bbbfe9848
jaschilling <jaschilling@arizona.edu>
2020-08-11 16:48:58 -0700

    added exploratory graphs

--- 2020-08-11/2020-08-11.R
+++ 2020-08-11/2020-08-11.R
@@ -39,3 +39,56 @@ skim(avatar.data)
 avatar.data %>% filter(is.na(imdb_rating)) %>% select(book, chapter) %>% unique(.)
 
 # Water - The Siege of the North, Part 2 is missing an IMDB Rating
+
+# Put the books in order from 1st to 3rd
+avatar.data <- avatar.data %>% mutate(book = factor(book, levels=unique(book[order(book_num)]), ordered=TRUE))
+
+# Exploratory graphs
+ggplot(avatar.data %>%
+         filter(!is.na(imdb_rating)) %>%
+         select(director, book, chapter, chapter_num, imdb_rating) %>%
+         unique(.) ) +
+  geom_point(aes(x = chapter_num, y = imdb_rating, color = director)) +
+  facet_grid(book ~ director) +
+  theme_bw()
+
+ggplot(avatar.data %>%
+  filter(!is.na(imdb_rating)) %>%
+  select(director, chapter, book, imdb_rating) %>%
+  unique(.) %>%
+  group_by(director, book) %>%
+  summarise(avg_rating = mean(imdb_rating),
+            num_chapters = n()) %>%
+  mutate(num_books = n_distinct(book))) +
+  geom_point(aes(x = num_chapters, y = avg_rating, size = num_books, color = director)) +
+  facet_grid(~ book) +
+  theme_bw()
+
+
+ggplot(avatar.data %>%
+  filter(!is.na(imdb_rating)) %>%
+  select(character, chapter, book, imdb_rating) %>%
+  unique(.) %>%
+  group_by(character, book) %>%
+  summarise(avg_rating = mean(imdb_rating),
+            num_chapters = n()) %>%
+  mutate(num_books = n_distinct(book)) %>%
+  filter(num_chapters > 5)) +
+  geom_point(aes(x = num_chapters, y = avg_rating, color = character)) +
+  facet_wrap(~ book) +
+  theme_bw()
+
+ggplot(avatar.data %>%
+         filter(!is.na(imdb_rating)) %>%
+         select(character, chapter_num, book, imdb_rating) %>%
+         unique(.) %>%
+         group_by(book, chapter_num) %>%
+         summarise(avg_rating = mean(imdb_rating),
+                   num_characters = n())) +
+  geom_point(aes(x = num_characters, y = avg_rating)) +
+  facet_wrap(~ book) +
+  theme_bw()
+
+
+
+
