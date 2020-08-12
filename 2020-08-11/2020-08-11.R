f312c88c7cf2689ce049d44683052f529712f5e2
Jenn Schilling <jathorne2@gmail.com>
2020-08-11 21:24:21 -0700

    added text analysis

--- 2020-08-11/2020-08-11.R
+++ 2020-08-11/2020-08-11.R
@@ -11,10 +11,9 @@ library(extrafont)
 library(cowplot)
 library(ggtext)
 library(tvthemes)
+library(tidytext)
 
 # Load  fonts
-import_avatar()
-
 loadfonts(quiet = TRUE)
 
 
@@ -47,30 +46,38 @@ avatar.data %>% filter(is.na(imdb_rating)) %>% select(book, chapter) %>% unique(
 avatar.data <- avatar.data %>% mutate(book = factor(book, levels=unique(book[order(book_num)]), ordered=TRUE))
 
 # Exploratory graphs
-ggplot(avatar.data %>%
-         filter(!is.na(imdb_rating)) %>%
-         select(director, book, chapter, chapter_num, imdb_rating) %>%
-         unique(.) ) +
+
+# Episode ratings by director and season
+avatar.data %>%
+  filter(!is.na(imdb_rating)) %>%
+  select(director, book, chapter, chapter_num, imdb_rating) %>%
+  unique(.) %>%
+ggplot() +
   geom_point(aes(x = chapter_num, y = imdb_rating, color = director)) +
   scale_color_avatar() +
   facet_grid(book ~ director) +
-  theme_avatar()
+  theme_avatar(title.font = "Slayer",
+               text.font = "Slayer")
 
-ggplot(avatar.data %>%
+# Average episode rating by director and season
+avatar.data %>%
   filter(!is.na(imdb_rating)) %>%
   select(director, chapter, book, imdb_rating) %>%
   unique(.) %>%
   group_by(director, book) %>%
   summarise(avg_rating = mean(imdb_rating),
             num_chapters = n()) %>%
-  mutate(num_books = n_distinct(book))) +
-  geom_point(aes(x = num_chapters, y = avg_rating, size = num_books, color = director)) +
+  mutate(num_books = n_distinct(book)) %>%
+ggplot() +
+  geom_point(aes(x = num_chapters, y = avg_rating, 
+                 size = num_books, color = director)) +
   facet_grid(~ book) +
   scale_color_avatar() +
-  theme_avatar()
+  theme_avatar(title.font = "Slayer",
+               text.font = "Slayer")
 
-
-ggplot(avatar.data %>%
+# Average rating by character and book
+avatar.data %>%
   filter(!is.na(imdb_rating)) %>%
   select(character, chapter, book, imdb_rating) %>%
   unique(.) %>%
@@ -78,22 +85,117 @@ ggplot(avatar.data %>%
   summarise(avg_rating = mean(imdb_rating),
             num_chapters = n()) %>%
   mutate(num_books = n_distinct(book)) %>%
-  filter(num_chapters > 5)) +
-  geom_jitter(aes(x = num_chapters, y = avg_rating, color = character), width = 1) +
+  filter(num_chapters > 5) %>%
+ggplot() +
+  geom_jitter(aes(x = num_chapters, y = avg_rating, color = character), 
+              width = 1) +
   facet_wrap(~ book) +
 #  scale_color_avatar() +
-  theme_avatar()
+  theme_avatar(title.font = "Slayer",
+               text.font = "Slayer")
 
-ggplot(avatar.data %>%
-         filter(!is.na(imdb_rating)) %>%
-         select(character, chapter_num, book, imdb_rating) %>%
-         unique(.) %>%
-         group_by(book, chapter_num) %>%
-         summarise(avg_rating = mean(imdb_rating),
-                   num_characters = n())) +
+# Ratings versus number of characters in an episode, by book
+avatar.data %>%
+  filter(!is.na(imdb_rating)) %>%
+  select(character, chapter_num, book, imdb_rating) %>%
+  unique(.) %>%
+  group_by(book, chapter_num) %>%
+  summarise(avg_rating = mean(imdb_rating),
+            num_characters = n()) %>%
+ggplot() +
   geom_jitter(aes(x = num_characters, y = avg_rating)) +
   facet_wrap(~ book) +
   scale_color_avatar() +
-  theme_avatar()
+  theme_avatar(title.font = "Slayer",
+               text.font = "Slayer")
 
 
+### Let's Do Some Text Analysis ####
+
+# Get words in each line, but first get characters who speak the most
+
+avatar.main.characters <- avatar.data %>%
+  group_by(character) %>%
+  summarise(num_lines = n()) %>%
+  ungroup() %>%
+  arrange(-num_lines) %>%
+  top_n(10) %>%
+  select(character) %>%
+  unique(.)
+
+main.characters <- tribble(
+  ~character, ~nation, ~gender,
+  'Aang', 'air', 'male',
+  'Sokka', 'water', 'male',
+  'Katara', 'water', 'female',
+  'Zuko', 'fire', 'male',
+  'Toph', 'earth', 'female',
+  'Iroh', 'fire', 'male',
+  'Azula', 'fire', 'female',
+  'Jet', 'earth', 'male',
+  'Suki', 'earth', 'female',
+  'Zhao', 'fire', 'male',
+)
+
+
+# Get words for main characters
+avatar.data.tokenize.main <- avatar.data %>%
+  inner_join(main.characters, by = "character") %>%
+  unnest_tokens(input = character_words,
+                output = word)
+
+# Get word counts by character, excluding stop words
+avatar.words.main <- avatar.data.tokenize.main %>%
+  anti_join(stop_words, by = "word") %>%
+  count(character, nation, gender, word, sort = TRUE) %>%
+  arrange(-n) 
+
+# Get total words by character
+avatar.total.words.main <- avatar.words.main %>%
+  group_by(character, nation, gender) %>%
+  summarise(total = sum(n))
+
+# Get word frequencies
+avatar.word.freq.main <- avatar.words.main %>%
+  left_join(., avatar.total.words.main, by = c("character", "gender", "nation")) %>%
+  mutate(freq = n / total)
+  
+ggplot(avatar.word.freq.main, aes(freq, fill = character)) +
+  geom_histogram(show.legend = FALSE) +
+  facet_wrap(~character, ncol = 2, scales = "free_y")
+
+# Who talks the most?
+ggplot(avatar.total.words.main) +
+  geom_bar(aes(x = reorder(character, total), y = total, fill = gender), 
+           stat = "identity") +
+  coord_flip() +
+  labs(y = "Number of Words",
+       x = "") +
+  scale_fill_avatar(palette = "EarthKingdom") +
+  theme_avatar(text.font = "Slayer")
+
+# What are they saying?
+avatar.word.freq.main %>%
+  filter(freq >= 0.005) %>%
+  filter(character == "Sokka") %>%
+  ggplot() +
+  geom_bar(aes(x = reorder(word, n), y = n, fill = nation), stat = "identity") +
+  coord_flip() +
+  labs(y = "Count",
+       x = "",
+       title = "Sokka") +
+  scale_fill_avatar(palette = "WaterTribe") +
+  theme_avatar(text.font = "Slayer")
+
+
+avatar.word.freq.main %>%
+  filter(freq >= 0.005) %>%
+  filter(character == "Aang") %>%
+ggplot() +
+  geom_bar(aes(x = reorder(word, n), y = n, fill = nation), stat = "identity") +
+  coord_flip() +
+  labs(y = "Count",
+       x = "",
+       title = "Aang") +
+  scale_fill_avatar(palette = "AirNomads") +
+  theme_avatar(text.font = "Slayer")
