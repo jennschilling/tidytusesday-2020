97ad5a8da455d9278f5103845cc78b54058d1efb
jaschilling <jaschilling@arizona.edu>
2020-08-17 16:48:32 -0700

    exploratory analysis

--- 2020-08-18/2020-08-18.R
+++ 2020-08-18/2020-08-18.R
@@ -25,9 +25,75 @@ plants <- tuesdata$plants
 
 skim(plants)
 
-# 500 records
-# 15 missing year_last_seen
+# 500 records, 500 species
+# 15 missing year_last_seen (3%)
 # red_list_category - Extinct, Extinct in the Wild
+# 75% have unknown action
+# 21% have unknown threat
 
 year_missing <- plants %>%
   filter(is.na(year_last_seen))
+
+#### Exploratory Plots ####
+
+# Number by country
+plants %>%
+  ggplot() +
+  geom_bar(aes(x = forcats::fct_infreq(country))) +
+  coord_flip()
+
+# Number by continent
+plants %>%
+  ggplot() +
+  geom_bar(aes(x = forcats::fct_infreq(continent))) +
+  coord_flip()
+
+# Number by year_last_seen
+plants %>%
+  ggplot() +
+  geom_bar(aes(x = forcats::fct_infreq(year_last_seen))) +
+  coord_flip()
+
+# Number by year_last_seen and continent
+plants %>%
+  ggplot() +
+  geom_bar(aes(x = forcats::fct_infreq(year_last_seen))) +
+  coord_flip() +
+  facet_wrap(~ continent)
+
+# Number by taxonomic group
+plants %>%
+  ggplot() +
+  geom_bar(aes(x = forcats::fct_infreq(group))) +
+  coord_flip()
+# Almost all are flowering plants
+
+# Number by Threat
+plants %>%
+  pivot_longer(threat_AA:threat_NA, names_to = "threat") %>%
+  separate(threat, c("threat", "threat_type"), sep = "_") %>%
+  select(-threat) %>% 
+  filter(value == 1) %>%
+  select(-value) %>%
+  ggplot() +
+  geom_bar(aes(x = forcats::fct_infreq(threat_type))) +
+  coord_flip()
+
+# Number by Action
+plants %>%
+  pivot_longer(action_LWP:action_NA, names_to = "action") %>%
+  separate(action, c("action", "action_type"), sep = "_") %>%
+  select(-action) %>% 
+  filter(value == 1) %>%
+  select(-value) %>%
+  ggplot() +
+  geom_bar(aes(x = forcats::fct_infreq(action_type))) +
+  coord_flip()
+
+#### Final Plot ####
+
+# Idea
+# Timeline by last year seen, continent, threat, action, group
+# Dots for each plant
+# Color/position by continent
+# How to encode group, threat, action ???
