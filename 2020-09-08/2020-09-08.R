9303651ac4aa6bfabc8bf3583fa9dc77ed30b2b4
Jenn Schilling <jathorne2@gmail.com>
2020-09-08 18:09:01 -0700

    initial commit

---
+++ 2020-09-08/2020-09-08.R
@@ -0,0 +1,20 @@
+# Author : Jenn Schilling
+# Title: #TidyTuesday Friends
+# Date: 9/08/2020
+
+
+#### Libraries ####
+library(friends)
+library(tidyverse)
+library(skimr)
+library(extrafont)
+library(cowplot)
+library(ggtext)
+library(janitor)
+
+# Load  fonts
+#font_import()
+loadfonts(device = "pdf")
+
+
+#### Get the Data ####
