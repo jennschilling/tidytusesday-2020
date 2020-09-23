dbd2137fa17c996f5f42d79d0e2fec0e18c46487
Jenn Schilling <jathorne2@gmail.com>
2020-09-22 18:59:54 -0700

    some data exploration

--- 2020-09-22/2020-09-22.R
+++ 2020-09-22/2020-09-22.R
@@ -25,7 +25,39 @@ members <- tuesdata$members
 
 skim(members)
 
-ggplot(members) +
-  geom_jitter(aes(x = year,
-                  y = age))
+members %>% 
+  filter(hired == TRUE) %>%
+  select(expedition_role) %>%
+  unique(.) %>%
+  View(.)
 
+members %>%
+  filter(hired == TRUE) %>%
+  group_by(year) %>%
+  count(age) %>%
+  View(.)
+
+staff <- members %>%
+  filter(hired == TRUE) %>%
+  mutate(age_bin = cut_interval(age, n = 5),
+         age_bin_name = case_when(
+           age_bin == "[15,25.4]" ~ "15 to 25",
+           age_bin == "(25.4,35.8]" ~ "26 to 35",
+           age_bin == "(35.8,46.2]" ~ "36 to 46",
+           age_bin == "(46.2,56.6]" ~ "47 to 56",
+           age_bin == "(56.6,67]" ~ "57 to 67",
+           TRUE ~ "Unknown"
+         ))
+
+staff %>%
+  ggplot() +
+  geom_bar(aes(x = age_bin_name,
+               fill = died))
+
+died <- members %>%
+  filter(died == TRUE)
+
+died %>%
+  ggplot() +
+  geom_bar(aes(x = hired,
+               fill = hired))
