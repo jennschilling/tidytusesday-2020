e74fc3b63f66f4811041e1d767e94330e7504dd2
Jenn Schilling <jathorne2@gmail.com>
2020-10-27 19:28:54 -0700

    added some data exploration and manipulation

--- 2020-10-27/2020-10-27.R
+++ 2020-10-27/2020-10-27.R
@@ -1,3 +1,4 @@
+
 # Author : Jenn Schilling
 # Title: #TidyTuesday Canadian Wind Turbines
 # Date: 10/27/2020
@@ -19,4 +20,36 @@ glimpse(wind_turbines)
 
 skim(wind_turbines)
 
+# Look at commissioning dates 
+
 table(wind_turbines$commissioning_date)
+
+wind_turbines %>%
+  ggplot() +
+  geom_bar(aes(x = commissioning_date))
+
+# Create single year numeric field
+wind_turbines <- wind_turbines %>%
+  mutate(commission_year = parse_number(commissioning_date))
+
+# Provinces
+
+table(wind_turbines$province_territory)
+
+# Manufacturers
+
+table(wind_turbines$manufacturer)
+
+ggplot(wind_turbines) +
+  geom_bar(aes(x = manufacturer)) # 5 manufacturers dwarf the rest, out of those 1 stands out
+
+# Turbine number in project
+
+table(wind_turbines$turbine_number_in_project)
+
+# Separate into turbine number and number of turbines in project
+wind_turbines <- wind_turbines %>%
+  separate(turbine_number_in_project, 
+           c("turbine_number_in_project", 
+             "number_turbines_in_project"),
+           sep = "/")
