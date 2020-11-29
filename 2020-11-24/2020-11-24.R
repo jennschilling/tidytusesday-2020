cfed4b5fa45a8279f365ee3877fa1de424279473
Jenn Schilling <jathorne2@gmail.com>
2020-11-29 12:04:13 -0700

    finalized plot for week 48

--- 2020-11-24/2020-11-24.R
+++ 2020-11-24/2020-11-24.R
@@ -7,6 +7,13 @@
 library(tidytuesdayR)
 library(tidyverse)
 library(skimr)
+library(showtext)
+library(sysfonts)
+
+# Add font
+font_add_google("Roboto")
+
+showtext_auto()
 
 #### Get the Data ####
 
@@ -35,7 +42,7 @@ hike_data_num <- hike_data %>%
 
 # Alternate way to get numeric - use parse_number
 # But then I miss out on the length_type of roundtrip vs one-way
-hike_data_num <- hike_data %>%
+hike_data_/num <- hike_data %>%
   mutate(gain = parse_number(gain),
          highpoint = parse_number(highpoint),
          rating = parse_number(rating),
@@ -112,7 +119,7 @@ hike_data_num_unnest %>%
 
 # Characteristics of high rated hikes vs. low rated hikes??
 
-hike_data_num_unnest %>%
+hike_data_num %>%
   mutate(rating_level = ifelse(rating == 0, 0,
                          ifelse(rating < 2, 1,
                          ifelse(rating < 3, 2,
@@ -121,6 +128,35 @@ hike_data_num_unnest %>%
   group_by(rating_level) %>%
   summarise(mean_length = mean(length),
             mean_gain = mean(gain),
-            mean_rating = mean(rating),
-            n = n())
+            n = n()) %>%
+  pivot_longer(mean_length:n) %>%
+  ggplot() +
+  geom_bar(aes(x = as.factor(rating_level), y = value, group = name),
+           stat = "identity") +
+  facet_wrap(~name,
+             scales = "free")
+
+
+#### Final Plot ####
+
+hike_data_num_unnest %>%
+  group_by(features) %>%
+  summarise(mean_rating = mean(rating)) %>%
+  ggplot(aes(x = mean_rating,
+             y = reorder(features, mean_rating))) +
+  geom_segment(aes(xend = 0,
+                   yend = reorder(features, mean_rating)),
+               size = 0.5) +
+  geom_point(size = 3,
+             color = "#228B22") +
+  labs(x = "Average Rating",
+       y = "Hike Features",
+       title = "Hikes in Washington that do not allow dogs, feature waterfalls, or have established campsites are rated highest, on average.",
+       caption = "TidyTuesday 24 Nov 2020 | Data: Washington Trails Association | Designer: Jenn Schilling | jennschilling.me") +
+  scale_x_continuous(limits = c(0, 5),
+                     expand = expansion(mult = c(0, .1))) +
+  theme_classic() +
+  theme(text = element_text(family = "Roboto"),
+        plot.title.position = "plot",
+        axis.ticks.y = element_blank())
 
